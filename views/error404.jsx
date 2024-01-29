c0cde36147b207dedf81b1903867f0527abe5861
Doidae <andyarredondo1224@gmail.com>
2024-01-29 18:59:17 -0500

    Added error page image

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404() {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <div>
+                    <img src="/images/coolCat.jpg" alt="Cat with glasses" style={{ width: '400px', height: '300px' }}/>
+                    <div>
+                        Photo by <a href="https://unsplash.com/@raouldroog?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Raoul Droog</a> on <a href="https://unsplash.com/photos/russian-blue-cat-wearing-yellow-sunglasses-yMSecCHsIBc?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+                    </div>
+                </div>
             </main>
         </Def>
     )

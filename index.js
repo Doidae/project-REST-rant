4281535ca42cbf18a2498704a095671b20fda84d
Doidae <andyarredondo1224@gmail.com>
2024-01-31 23:30:21 -0500

    Delete functionality implemented

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
+
 
 app.set('views', __dirname + '/views') // probably missed this, added on part 4 step 2?
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true}))
+app.use(methodOverride('_method'))
 
 app.use('/places', require('./controllers/places'))
 
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,38 @@
 const React = require('react')
 const Def = require('../default')
 
-function show() {
+function show(data) {
     return (
         <Def>
             <main>
-                <h1>Show Page</h1>
+                <div className='container'>
+                    <div className='row'> 
+                        <div className='col'>
+                            <img src={data.place.pic} alt={data.place.name} />
+                        </div>
+                        <div className='col'>
+                            <h1> { data.place.name } </h1>
+                            <h2>Rating</h2>
+                            <p id='showText'>Not Rated</p>
+                            <div>
+                                <h2>Description</h2>
+                                <p id='showText'>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</p>
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <h2>Comments</h2>
+                            <p id='showText'>No comments yet!</p>
+                        </div>
+                        <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
+                            Edit
+                        </a>
+                        <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
+                            <button type='submit' className='btn btn-danger'>
+                                Delete
+                            </button>
+                        </form>
+                    </div>
+                </div>
             </main>
         </Def>
     )

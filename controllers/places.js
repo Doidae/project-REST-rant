1d0276d2c6e85ff28731bd71a653e0f0165e6296
Doidae <andyarredondo1224@gmail.com>
2024-02-05 21:56:40 -0500

    Fixed the show page and links

--- controllers/places.js
+++ controllers/places.js
@@ -31,7 +31,14 @@ router.post('/', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-    res.send('GET /places/:id stub')
+    db.Place.findById(req.params.id)
+    .then(place => {
+        res.render('places/show', { place } )
+    })
+    .catch(err => {
+        console.log('err', err)
+        res.render('error404')
+    })
 })
 
 router.delete('/:id', (req, res) => {
